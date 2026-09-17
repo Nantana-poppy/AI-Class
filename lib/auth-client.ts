ca6440a6acc4560cb1ea63fe891316f30bdd9ca2
nantana-poppy <ppscc1003@gmail.com>
2026-09-17 18:30:31 +0700

    test deploy2

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -1,8 +1,6 @@
 import { createAuthClient } from "better-auth/react";
 
-export const authClient = createAuthClient({
-  baseURL: process.env.NEXT_PUBLIC_APP_URL || (typeof window !== "undefined" ? window.location.origin : undefined),
-});
+export const authClient = createAuthClient();
 
 export const { signIn, signUp, signOut, useSession } = authClient;
 
